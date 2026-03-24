9ec01a86ea506852c8441981a3cbf4941e7d0f61
Marcio Briller <mbriller23@gmail.com>
2026-03-24 16:59:37 +0000

    ajuste seleção dinâmica com filtros

--- srv/service.js
+++ srv/service.js
@@ -9,7 +9,8 @@ module.exports = cds.service.impl(async function () {
     })
 
     this.on('RequisicaoCadastro', async (req) => { 
-
+        
+        const filtros = {}
         const { Cadastro } = this.entities;
         const VariavelID = req?.data?.ID;
         const VariavelCpf = req?.data?.cpf;
@@ -19,11 +20,27 @@ module.exports = cds.service.impl(async function () {
             return req.error(400, 'Não foi digitado o campo chave');            
         }
 
-        const cadastro = await SELECT.from(Cadastro).where({ 
-            ID: VariavelID,
-            cpf: VariavelCpf,
-            nome: VariavelNome })
+        if (VariavelID) {
+            filtros.ID = VariavelID
+        }
+
+        if (VariavelCpf) {
+            filtros.cpf = VariavelCpf
+        }
+
+        if (VariavelNome) {
+            filtros.nome = VariavelNome
+        }
+
+        // selecionando dinamicamente de acordo com os filtros acima
+        const cadastro = await SELECT.from(Cadastro).where(filtros) 
         
+        // selecionando de maneira estática: ID, CPF e NOME
+//        const cadastro = await SELECT.from(Cadastro).where({ 
+//            ID: VariavelID,
+//            cpf: VariavelCpf,
+//            nome: VariavelNome })
+
         if (!cadastro || cadastro.length === 0) {
             return req.error(400, 'Dados não encontrados');            
         } else {
